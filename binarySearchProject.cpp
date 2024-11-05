fe0bfed698c2b2bfc8dff065039f7704718bf866
pikarichu <pikarichu@gmail.com>
2024-11-04 20:59:14 -0500

    Finished adding the random number array

--- binarySearchProject.cpp
+++ binarySearchProject.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <random>
+
+using namespace std;
+
 
 int recursiveBinarySearch(const std::vector<int>& arr, int target, int low, int high) {
     if (low <= high) {
@@ -23,8 +27,8 @@ int recursiveBinarySearch(const std::vector<int>& arr, int target, int low, int 
 
 int iterativeBinarySearch(const std::vector<int>& arr, int target) {
     int low = 0;
-    int high = arr.size() - 1;
-
+    //int high = arr.size() - 1;
+    int high = 10;
     while (low <= high) {
         int mid = low + (high - low) / 2;
 
@@ -55,35 +59,65 @@ int sequentialSearch(const std::vector<int>& arr, int target) {
 }
 
 int main() {
-    std::vector<int> arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    
+    vector<int> arr = { };
+    mt19937 rng(std::random_device{}());
+    uniform_int_distribution<int> distribution(1, 100);
+    //arr.begin();
+    for (int i = 0; i < 10; ++i) {
+    
+        int random_number = distribution(rng);
+        arr.insert(arr.begin(), random_number);
+    }
+    //std::vector<int> arr = { 6, 5, 7, 8, 1, 3, 4, 2, 9, 10 };
+    sort(arr.begin(), arr.end());
+
     int target = 6;
+    for (int i = 0; i < 10; ++i)
+    {
+        cout << arr[i] << ", ";
+    }
+    cout << endl;
 
     int result = recursiveBinarySearch(arr, target, 0, arr.size() - 1);
 
     if (result != -1) {
-        std::cout << "Element found at index: " << result << std::endl;
+        std::cout << target << "Element found at index: " << result << std::endl;
     }
     else {
-        std::cout << "Element not found in the array." << std::endl;
+        std::cout << target << "Element not found in the array." << std::endl;
 
     }
+    
+    /*for (int i = 0; i < 100; ++i)
+    {
+        int result = recursiveBinarySearch(arr, i, 0, arr.size() - 1);
+
+        if (result != -1) {
+            std::cout << i << ": Element found at index: " << result << std::endl;
+        }
+        else {
+            std::cout << i << ": Element not found in the array." << std::endl;
+
+        }
+    }*/
 
     result = iterativeBinarySearch(arr, target);
 
     if (result != -1) {
-        std::cout << "Element found at index: " << result << std::endl;
+        std::cout << target << "Element found at index: " << result << std::endl;
     }
     else {
-        std::cout << "Element not found in the array." << std::endl;
+        std::cout << target << "Element not found in the array." << std::endl;
     }
 
     result = sequentialSearch(arr, target);
 
     if (result != -1) {
-        std::cout << "Element found at index: " << result << std::endl;
+        std::cout << target << "Element found at index: " << result << std::endl;
     }
     else {
-        std::cout << "Element not found in the array." << std::endl;
+        std::cout << target << "Element not found in the array." << std::endl;
     }
 
     return 0;
